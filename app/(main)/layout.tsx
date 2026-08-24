bb2d865d63e168f4c4d7db743ffaa6f94104c037
ReactDev81 <reactdev81@gmail.com>
2026-08-24 17:13:32 +0530

    first commit

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,16 @@
+import { Footer } from "@/components/layout/Footer";
+import { Header } from "@/components/layout/Header";
+import { Toaster } from "sonner";
+import Head from "next/head";
+import { useSettings } from "@/context/settingsContext";
+
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <Header />
+            <main className="flex-1 min-h-screen px-5 sm:px-5 py-5 sm:py-5 mx-auto w-full">{children}</main>
+            <Toaster richColors position="top-right" />
+            <Footer />
+        </>
+    );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+import { Cormorant_Garamond, Manrope, Geist_Mono } from "next/font/google";
+import { cn } from "@/lib/utils";
+import "./globals.css";
+import { Providers } from "./providers";
+import Head from "next/head";
+
+const cormorant = Cormorant_Garamond({
+    subsets: ['latin'],
+    weight: ['300', '400', '500', '600', '700'],
+    variable: '--font-headings',
+})
+
+const manrope = Manrope({
+    subsets: ['latin'],
+    variable: '--font-sans',
+})
+
+const fontMono = Geist_Mono({
+    subsets: ["latin"],
+    variable: "--font-mono",
+})
+
+export const metadata: Metadata = {
+    title: "CMC Telehealth - Doctor",
+    description: "A Progressive Web App for Doctors in Cmc Telehealth",
+    manifest: "/manifest.webmanifest",
+    icons: {
+        icon: "/favicon.ico",
+    },
+};
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html
+            lang="en"
+            className={cn("antialiased", fontMono.variable, "font-sans", manrope.variable, cormorant.variable)}
+            suppressHydrationWarning
+        >
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <body className="min-h-full flex flex-col" suppressHydrationWarning>
+                <Providers>
+                    {children}
+                </Providers>
+            </body>
+        </html>
+    );
+}
